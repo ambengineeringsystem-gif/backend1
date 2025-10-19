c801f3db6728cd05bf42e500e66e86653e8a71fa
AMB-Engineering <ambengineeringsystem@gmail.com>
2025-10-19 23:22:32 +0200

    Delete api directory

--- api/health.js
+++
@@ -1,9 +0,0 @@
-const { buildClient } = require('../lib/s3');
-module.exports = async (req, res) => {
-  try {
-    buildClient(process.env);
-    return res.json({ status: 'ok' });
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
-};
